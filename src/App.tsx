f90be690eceb367e2a00729bab2601e2738a08e2
Celio Junior <demon.junior13@gmail.com>
2025-12-21 21:18:14 -0300

    initial component sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Sidebar from './components/Sidebar';
+
 function App() {
-  return <h1>my dashboard</h1>;
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,17 @@
+export default function Sidebar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="">Dashboard</a>
+        </li>
+        <li>
+          <a href="">Posts</a>
+        </li>
+        <li>
+          <a href="">Config</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
